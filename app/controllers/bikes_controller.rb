adbecf731974bddbddf4e910cbd3f29033216565
Haadiiii <hammali99@gmail.com>
2023-01-05 16:52:06 +0500

    resolve linters

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,5 +1,5 @@
 class BikesController < ApplicationController
-  before_action :set_bike, only: %i[ show update destroy ]
+  before_action :set_bike, only: %i[show update destroy]
 
   # GET /bikes
   def index
@@ -30,13 +30,14 @@ class BikesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_bike
-      @bike = Bike.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def bike_params
-      params.require(:bike).permit(:name, :description, :brand, :bike_type, :daily_rate)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_bike
+    @bike = Bike.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def bike_params
+    params.require(:bike).permit(:name, :description, :brand, :bike_type, :daily_rate)
+  end
 end
--- test/controllers/bikes_controller_test.rb
+++ test/controllers/bikes_controller_test.rb
@@ -1,35 +1,35 @@
-require "test_helper"
+require 'test_helper'
 
 class BikesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @bike = bikes(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get bikes_url, as: :json
     assert_response :success
   end
 
-  test "should create bike" do
-    assert_difference("Bike.count") do
-      post bikes_url, params: { bike: {  } }, as: :json
+  test 'should create bike' do
+    assert_difference('Bike.count') do
+      post bikes_url, params: { bike: {} }, as: :json
     end
 
     assert_response :created
   end
 
-  test "should show bike" do
+  test 'should show bike' do
     get bike_url(@bike), as: :json
     assert_response :success
   end
 
-  test "should update bike" do
-    patch bike_url(@bike), params: { bike: {  } }, as: :json
+  test 'should update bike' do
+    patch bike_url(@bike), params: { bike: {} }, as: :json
     assert_response :success
   end
 
-  test "should destroy bike" do
-    assert_difference("Bike.count", -1) do
+  test 'should destroy bike' do
+    assert_difference('Bike.count', -1) do
       delete bike_url(@bike), as: :json
     end
 
