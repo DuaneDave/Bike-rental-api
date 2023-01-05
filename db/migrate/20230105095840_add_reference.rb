4dd80b7100a4fc5182b8d38f3f4cb1601508550c
Ernest Anyewe Adonu <principlesbrothers@yahoo.com>
2023-01-05 10:33:47 +0000

    Add user and bike reference to reservation

---
+++ db/migrate/20230105095840_add_reference.rb
@@ -0,0 +1,6 @@
+class AddReference < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :reservations, :user, foreign_key: true
+    add_reference :reservations, :bike, foreign_key: true
+  end
+end
