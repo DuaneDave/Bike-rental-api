b87ca50f1c1e35e87158a06b075b96815ee1a72d
Haadiiii <hammali99@gmail.com>
2023-01-18 00:43:29 +0500

    Add tests for bikes

--- spec/integration/bikes_spec.rb
+++ spec/integration/bikes_spec.rb
@@ -13,18 +13,18 @@ describe 'Bikes API' do
           description: { type: :string },
           brand: { type: :string },
           daily_rate: { type: :decimal },
+          user_id: { type: :integer },
           images: { type: :hstore },
-          color: { type: :array },
-          user_id: { type: :integer }
+          color: { type: :array }
         },
-        required: %w[name bike_type description brand daily_rate images color user_id]
+        required: %w[name bike_type description brand daily_rate user_id images color]
       }
 
       response '201', 'bike created' do
         let(:user) { User.create!(name: 'name', email: 'email.com', password: 'password') }
         let(:bike) do
-          { name: 'Bike', bike_type: 'Mountain', description: 'This is a bike', brand: 'Trek', daily_rate: 10.0,
-            images: { blue: 'da' }, color: ['abc'], user_id: user.id }
+          { name: 'Bike', bike_type: 'Mountain', description: 'This is a bike', brand: 'Trek', daily_rate: 10.0, user_id: user.id,
+            images: { blue: 'da' }, color: ['abc'] }
         end
         run_test!
       end
@@ -38,7 +38,6 @@ describe 'Bikes API' do
     get 'Retrieves bikes' do
       tags 'Bikes'
       produces 'application/json', 'application/xml'
-
       response '200', 'bikes found' do
         schema type: :array,
                items: {
@@ -50,37 +49,29 @@ describe 'Bikes API' do
                    description: { type: :string },
                    brand: { type: :string },
                    daily_rate: { type: :decimal },
+                   user_id: { type: :integer },
                    images: { type: :hstore },
-                   color: { type: :array },
-                   user_id: { type: :integer }
+                   color: { type: :array }
                  }
                }
         run_test!
       end
     end
+  end
 
-    path '/api/v1/bikes/{id}' do
-      delete 'Deletes bike' do
-        tags 'Bikes'
-        produces 'application/json', 'application/xml'
-        response '204', 'bikes deleted' do
-          schema type: :array,
-                 items: {
-                   type: :object,
-                   properties: {
-                     id: { type: :integer },
-                     name: { type: :string },
-                     bike_type: { type: :string },
-                     description: { type: :string },
-                     brand: { type: :string },
-                     daily_rate: { type: :decimal },
-                     images: { type: :hstore },
-                     color: { type: :array },
-                     user_id: { type: :integer }
-                   }
-                 }
-          run_test!
+  path '/api/v1/bikes/{id}' do
+    delete 'Deletes a bike' do
+      tags 'Bikes'
+      produces 'application/json', 'application/xml'
+      parameter name: :id, in: :path, type: :string
+
+      response '204', 'bike deleted' do
+        let(:user) { User.create!(name: 'name', email: 'email.com', password: 'password') }
+        let(:id) do
+          Bike.create!(name: 'name', bike_type: 'type', description: 'desc', brand: 'brand',
+                       daily_rate: 10.0, user_id: user.id, images: { blue: 'da' }, color: ['abc']).id
         end
+        run_test!
       end
     end
   end
