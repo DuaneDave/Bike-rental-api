2ea5c697b631da5c102b81a254782577785b3353
Haadiiii <hammali99@gmail.com>
2023-01-18 00:43:44 +0500

    Add tests for reservation test

--- spec/integration/reservations_spec.rb
+++ spec/integration/reservations_spec.rb
@@ -61,19 +61,19 @@ describe 'Reservations API' do
       delete 'Deletes reservation' do
         tags 'Reservations'
         produces 'application/json', 'application/xml'
-        response '204', 'reservations deleted' do
-          schema type: :array,
-                 items: {
-                   type: :object,
-                   properties: {
-                     id: { type: :integer },
-                     bike_id: { type: :integer },
-                     user_id: { type: :integer },
-                     reservation_date: { type: :string },
-                     due_date: { type: :string },
-                     city: { type: :string }
-                   }
-                 }
+        parameter name: :id, in: :path, type: :string
+
+        response '204', 'reservation deleted' do
+          let(:user) { User.create!(name: 'name', email: 'email.com', password: 'password') }
+          let(:bike) do
+            Bike.create(name: 'bikeone', bike_type: 'one', description: 'dasdas', brand: 'dasd',
+                        daily_rate: 231.23, color: ['dasd'], images: { blue: 'dasda' }, user_id: user.id)
+          end
+          let(:reservation) do
+            Reservation.create(bike_id: bike.id, user_id: user.id, reservation_date: '2021-03-01',
+                               due_date: '2021-03-03', city: 'Bogota')
+          end
+          let(:id) { reservation.id }
           run_test!
         end
       end
